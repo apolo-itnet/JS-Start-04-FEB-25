1fd3c4f434614bdc1f0311f3413610c89a68a405
Apolo Barua Apurbo <apolo.itnet@gmail.com>
2025-02-14 20:09:34 +0600

    update :~$ git commit -m "Fixed conditional statement in main.js" [main 4a4a4a4] Fixed conditional statement in main.js 1 file changed, 1 insertion(+), 1 deletion(-) update :~$ git push origin main Enumerating objects: 3, done. Counting objects: 3, done. Delta compression using up-to-date chain. Compressing objects: 100% (2/2), done. Writing objects: 100% (3/3), 246 bytes | 246.00 Ki B/s Total 3 (delta 0), reused 0 (delta 0), pack-reused 0 To https://github.com/username/repository.git 4a4a4a4..4a4a4a4  main -> main

--- Module-21/conditional.js
+++ Module-21/conditional.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function isEven(number){
   if (number % 2 === 0) {
     return true;
@@ -21,27 +20,3 @@ function isOdd(number){
 
 console.log(isOdd(10))
 console.log(isOdd(7))
-=======
-function isEven(number){
-  if (number % 2 === 0) {
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-
-console.log(isEven(5))
-console.log(isEven(110))
-
-
-function isOdd(number){
-  if (number % 2 === 1) {
-    return true;
-  } 
-  return false;
-}
-
-console.log(isOdd(10))
-console.log(isOdd(7))
->>>>>>> 2954b252eb227cb3ccc56c20266e407edd9ba264
