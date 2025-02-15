68e7b6344dcd655de0d7b6eb370d68ced4df0436
Apolo Barua Apurbo <apolo.itnet@gmail.com>
2025-02-15 21:30:25 +0600

    js-s-module21-various-parameter-update

--- Module-21/various-parameter.js
+++ Module-21/various-parameter.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * for a given string tell me whether it has even number of characers or not 
  * 
@@ -31,38 +30,4 @@ function doubleOrTriple(number, doDouble){
 }
 
 console.log(doubleOrTriple(5, true));
-=======
-/**
- * for a given string tell me whether it has even number of characers or not 
- * 
- */
-
-function evenSizedString(str){
-  const size = str.length;
-  console.log(str, size)
-  if (size % 2 === 0) {
-    console.log('even size')
-  } 
-  else {
-    console.log('odd size')
-  }
-
-}
-evenSizedString("chittagong")
-evenSizedString("sajek")
-
-
-function doubleOrTriple(number, doDouble){
-  if (doDouble === true) { 
-    const result = number * 2;
-    return result;
-  }
-  else {
-    const result = number * 3;
-    return result;
-  }
-}
-
-console.log(doubleOrTriple(5, true));
->>>>>>> 2954b252eb227cb3ccc56c20266e407edd9ba264
 console.log(doubleOrTriple(5, false));
