65600fca8141562371329a16f411203c7b029a79
Apolo Barua Apurbo <apolo.itnet@gmail.com>
2025-03-08 13:11:44 +0600

    javascript-practice-update-2times-08/03/25

---
+++ Module-32/data.js
@@ -0,0 +1,49 @@
+//null
+
+// let name = null;
+// console.log(name)
+// console.log(typeof name)
+
+
+
+//truthy-falsy
+
+// const name = "" //empty string hole falsy value 
+// const name = true; //bolean value hole truthy value
+// const name = 0; //0 hole falsy value
+// const name = -1; //negative hole falsy value
+// const name = 1; //number hole falsy value
+// const name = [];
+// const name = "Apolo Barua";
+
+
+// if (name){
+//   // console.log('truthy value');
+// }
+// else {
+//   // console.log('falsy value');
+// }
+
+
+// = equal
+
+// const num1 = 10;
+// const num2 = "10";
+
+// if (num1 == num2){
+//   console.log('equal value');
+// }
+// else {
+//   console.log('not equal value');
+// }
+
+
+// scope
+
+function sum(num1, num2){
+  const result = num1 + num2;
+
+  // console.log(result)
+}
+
+sum(10,20)
